7fe3208f2618e964818ba73f5a5b34210f9b2e80
Gregorio Galante <me@gregoriogalante.com>
2022-11-24 21:55:58 +0100

    General update

--- lib/lato/engine.rb
+++ lib/lato/engine.rb
@@ -5,5 +5,9 @@ module Lato
     initializer 'lato.importmap', before: 'importmap' do |app|
       app.config.importmap.paths << root.join('config/importmap.rb')
     end
+
+    initializer "lato.precompile" do |app|
+      app.config.assets.precompile << "lato_manifest.js"
+    end
   end
 end
