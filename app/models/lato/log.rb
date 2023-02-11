a8410c9a802cca1ad2b1b969c3291c2d29ffa19d
Gregorio Galante <me@gregoriogalante.com>
2023-02-11 21:24:15 +0100

    Fix log of user signin

--- app/models/lato/log.rb
+++ app/models/lato/log.rb
@@ -1,12 +1,5 @@
 module Lato
   module Log
-    # This module is used to add log to the application.
-    # Log are used to track user actions without sensitive data.
-    # Log should not be destroyed.
-    before_destroy do
-      throw :abort
-    end
-
     def self.table_name_prefix
       'lato_log_'
     end
--- app/models/lato/log/user_signin.rb
+++ app/models/lato/log/user_signin.rb
@@ -1,8 +1,17 @@
 module Lato
-  class Log::UserSignin < ApplicationRecord
-    # Relations
-    ##
+  module Log
+    class UserSignin < ApplicationRecord
+      # Relations
+      ##
 
-    belongs_to :lato_user, class_name: 'Lato::User', foreign_key: :lato_user_id, optional: true
+      belongs_to :lato_user, class_name: 'Lato::User', foreign_key: :lato_user_id, optional: true
+
+      # Hooks
+      ##
+
+      before_destroy do
+        throw :abort
+      end
+    end
   end
 end
