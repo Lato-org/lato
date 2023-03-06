a660a6b7bfc70aaed391cb17abe29653ecc3fb00
Gregorio Galante <me@gregoriogalante.com>
2023-03-06 21:42:00 +0100

    Make sidebar auto-close on mobile on page change

--- app/assets/javascripts/lato/application.js
+++ app/assets/javascripts/lato/application.js
@@ -4,3 +4,8 @@ import "@hotwired/turbo-rails"
 import "bootstrap"
 // Import controllers (stimulus rails)
 import "controllers"
+
+// Close sidebar before page change with turbo
+document.addEventListener("turbo:before-visit", () => {
+  document.body.classList.remove('aside-open')
+})
