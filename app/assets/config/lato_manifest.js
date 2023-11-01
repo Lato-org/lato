f1e4b01d4201e507cee41b7056b726127550dd5a
Gregorio Galante <me@gregoriogalante.com>
2023-11-01 09:19:46 +0100

    Updates

--- app/assets/config/lato_manifest.js
+++ app/assets/config/lato_manifest.js
@@ -1,5 +1,5 @@
-//= link_directory ../images/lato .jpg
 //= link_directory ../stylesheets/lato .css
+//= link_directory ../images/lato .jpg
 //= link_tree ../javascripts/lato .js
 //#= link popper.js
 //= link bootstrap.js
