93aae268177d31ad62c6455adf90eb1eea2eff46
Gregorio Galante <me@gregoriogalante.com>
2025-04-15 18:08:22 +0200

    Added generated doc

---
+++ doc/assets/lato/controllers/lato_tooltip_controller-00b907dc82b798c60949813ec2f35772c4336db3cc5e021b5f3413b648bae1b8.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  /**
+   * Stimulus
+   */
+
+  connect() {
+    this.tooltip = new bootstrap.Tooltip(this.element)
+
+    // HACK: Force tooltip to be closed when a boostrap modal is opened/closed
+    const modals = document.querySelectorAll('.modal')
+    modals.forEach((modal) => {
+      modal.addEventListener('show.bs.modal', () => {
+        setTimeout(() => this.tooltip.hide(), 0)
+      })
+      modal.addEventListener('hide.bs.modal', () => {
+        setTimeout(() => this.tooltip.hide(), 0)
+      })
+    })
+  }
+
+  disconnect() {
+    this.tooltip.dispose()
+  }
+};
