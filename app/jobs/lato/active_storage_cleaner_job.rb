d8cc38231bd7ae1754088b036871cff6bc5cc405
Gregorio Galante <me@gregoriogalante.com>
2025-05-18 08:29:41 +0200

    Make active storage cleaner job usable from operations

--- app/jobs/lato/active_storage_cleaner_job.rb
+++ app/jobs/lato/active_storage_cleaner_job.rb
@@ -1,9 +1,16 @@
 module Lato
   class ActiveStorageCleanerJob < Lato::ApplicationJob
-    def perform
+    def perform(params = {})
+      update_operation_percentage(0)
+
       # delete all active storage blobs that are not attached to any record with more than 12 hours of life
-      ActiveStorage::Blob.unattached.where('active_storage_blobs.created_at < ?', 12.hours.ago).find_in_batches do |blobs|
+      query = ActiveStorage::Blob.where('active_storage_blobs.created_at < ?', 12.hours.ago)
+      totals = query.count
+      runned = 0
+      query.find_in_batches do |blobs|
         blobs.each(&:purge_later)
+        runned += blobs.size
+        update_operation_percentage((runned.to_f / totals * 100).round)
       end
 
       # delete all empty folders in active storage local service (if exists)
