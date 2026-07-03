d745a84749a64b3036a1c414e485631792aba948
Gregorio Galante <me@gregoriogalante.com>
2026-07-03 08:14:55 +0200

    Fix lato tooltip on click

--- app/assets/javascripts/lato/controllers/lato_tooltip_controller.js
+++ app/assets/javascripts/lato/controllers/lato_tooltip_controller.js
@@ -8,6 +8,10 @@ export default class extends Controller {
   connect() {
     this.tooltip = new bootstrap.Tooltip(this.element)
 
+    // Always close the tooltip when its trigger element is clicked, since the
+    // click may move the element and leave the tooltip stuck in the old position
+    this.element.addEventListener('click', () => this.tooltip.hide())
+
     // HACK: Force tooltip to be closed when a boostrap modal is opened/closed
     const modals = document.querySelectorAll('.modal')
     modals.forEach((modal) => {
